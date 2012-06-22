3ca5628788bd29e086640e89913484cea95c62eb
Gerald L. Hevener Jr <hevenerg@marshall.edu>
2012-06-22 01:47:14 -0400

    Update metadata.* for avahi-daemon ckbk

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "jackl0phty@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures avahi-daemon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.8"
+version          "0.0.10"
