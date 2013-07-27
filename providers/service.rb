5e7994dab03527b110d37423b86c0aefe221a747
Brian Hartsock <brian.hartsock@gmail.com>
2013-07-27 16:53:13 -0400

    Add LWRP for Avahi Services

---
+++ providers/service.rb
@@ -0,0 +1,27 @@
+def whyrun_supported?
+  true
+end
+
+use_inline_resources
+
+action :create do
+  template "/etc/avahi/services/#{new_resource.name}.service" do
+    source 'service.erb'
+    cookbook 'avahi-daemon'
+    mode new_resource.mode
+    owner new_resource.owner
+    group new_resource.group
+
+    variables({
+      :name => new_resource.name,
+      :replace_wildcards => new_resource.replace_wildcards,
+      :services => new_resource.services
+    })
+  end
+end
+
+action :delete do
+  template "/etc/avahi/services/#{new_resource.name}.service" do
+    action :delete
+  end
+end
